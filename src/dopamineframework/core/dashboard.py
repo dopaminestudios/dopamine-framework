3cbc6d94dcca6e155473a18c5521aa95ce1e2bcf
likerofturtles <singhdivyam664@gmail.com>
2026-02-13 00:29:42 +0530

    fix: Added relative import fix in dashboard.py to fix this error that occurs when attempting to load internal cog: ERROR: Failed to start the bot: Extension 'dopamineframework.ext.diagnostics' raised an error: ModuleNotFoundError: No module named 'src'

--- src/dopamineframework/core/dashboard.py
+++ src/dopamineframework/core/dashboard.py
@@ -4,8 +4,8 @@ import discord
 import signal
 import asyncio
 from typing import TYPE_CHECKING
-from src.dopamineframework.bot import Bot
-from src.dopamineframework.core.commands_registry import CommandRegistry
+from ..bot import Bot
+from ..core.commands_registry import CommandRegistry
 signal_handler = Bot.signal_handler
 restart_bot = Bot.restart_bot
 
